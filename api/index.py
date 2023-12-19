963aac418fa4d11222ea0360d064cfe1c42ffdcc
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-20 00:55:04 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,3 +16,29 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
         return
+
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=getenv("BOT_TOKEN"))
+dp = Dispatcher()
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ\nя пришлю тебе твое сообщение')
+
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text = 'Данный тип апдейтов не поддерживается методом send_copy')
