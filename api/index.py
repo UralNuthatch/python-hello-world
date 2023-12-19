d273ba1722cc4a7ca77767ec864dc673fd014a9c
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-20 00:33:47 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,11 @@
+import sys
+from os import getenv
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
+
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
