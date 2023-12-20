21da9805360fd234678346c6f3b076f17be35b5c
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-21 01:05:16 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ from aiogram.filters import Command, CommandStart
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from os import getenv
 from aiohttp import web
-from http.server import BaseHTTPRequestHandler
+from fastapi import FastAPI
 
 
 
@@ -15,63 +15,11 @@ WEB_SERVER_HOST = "http://python-hello-world-uralnuthatchs-projects.vercel.app/"
 # Порты сервера: 
 WEB_SERVER_PORT = 8350
 # Путь к маршруту вебхука, по которому Telegram будет отправлять запросы
-WEBHOOK_PATH = ""
+WEBHOOK_PATH = f"/bot/{getenv("BOT_TOKEN")}"
 # Базовый URL-адрес вебхука, который будет исп-ся для создания URL-адреса вебхука для Telegram
 BASE_WEBHOOK_URL = f"{WEB_SERVER_HOST}{WEBHOOK_PATH}"
 # На сервере только IPv6 (аналог ip4: 0.0.0.0).
 WEBAPP_HOST = "0.0.0.0"
 
 
-async def on_startup(bot: Bot) -> None:
-    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}")
 
-def main() -> None:
-
-    # Создаем объекты бота и диспетчера
-    bot = Bot(token=getenv("BOT_TOKEN"), parse_mode='HTML')
-    dp = Dispatcher()
-
-    # Этот хэндлер будет срабатывать на команду "/start"
-    @dp.message(Command(commands=["start"]))
-    async def process_start_command(message: Message):
-        await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
-
-    # Этот хэндлер будет срабатывать на команду "/help"
-    @dp.message(Command(commands=['help']))
-    async def process_help_command(message: Message):
-        await message.answer('Напиши мне что-нибудь и в ответ\nя пришлю тебе твое сообщение')
-
-    # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения
-    # кроме команд "/start" и "/help"
-    @dp.message()
-    async def send_echo(message: Message):
-        try:
-            await message.send_copy(chat_id=message.chat.id)
-        except TypeError:
-            await message.reply(text = 'Данный тип апдейтов не поддерживается методом send_copy')
-
-    # Register startup hook to initialize webhook
-    dp.startup.register(on_startup)
-
-    # Create aiohttp.web.Application instance
-    app = web.Application()
-
-    # Create an instance of request handler
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot
-    )
-
-    # Register webhook handler on application
-    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-
-    # Mount dispatcher startup and shutdown hooks to aiohttp application
-    setup_application(app, dp, bot=bot)
-
-    # Запускаем веб-сервер
-    web.run_app(app, host=WEBAPP_HOST)
-
-
-    #if __name__ == '__main__':
-        #logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-main()
