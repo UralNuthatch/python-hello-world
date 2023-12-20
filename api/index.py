170d099507c11fc6d29be4a8aaf3b84d1921e2c3
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-21 01:26:08 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -22,4 +22,46 @@ BASE_WEBHOOK_URL = f"{WEB_SERVER_HOST}{WEBHOOK_PATH}"
 WEBAPP_HOST = "0.0.0.0"
 
 
+def setup_app():
+      
+    app = FastAPI(openapi_url=None, docs_url=None, redoc_url=NOne)
+  
+
+    # Создаем объекты бота и диспетчера
+    bot = Bot(token=getenv("BOT_TOKEN"), parse_mode='HTML')
+    dp = Dispatcher()
+
+    # Этот хэндлер будет срабатывать на команду "/start"
+    @dp.message(Command(commands=["start"]))
+    async def process_start_command(message: Message):
+        await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+    # Этот хэндлер будет срабатывать на команду "/help"
+    @dp.message(Command(commands=['help']))
+    async def process_help_command(message: Message):
+        await message.answer('Напиши мне что-нибудь и в ответ\nя пришлю тебе твое сообщение')
+
+    # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения
+    # кроме команд "/start" и "/help"
+    @dp.message()
+    async def send_echo(message: Message):
+        try:
+            await message.send_copy(chat_id=message.chat.id)
+        except TypeError:
+            await message.reply(text = 'Данный тип апдейтов не поддерживается методом send_copy')
+
+    @app.post(WEBHOOK_PATH)
+    async def bot_webhook(update: dict):
+        res = await dp.feed_webhook_update(bot, update)
+        return res
+
+    @app.get("/")
+    async def setup():
+        await bot.set_webhook(url=BASE_WEBHOOK_URL, drop_pending_updates=True)
+        return "Webhook Updated"
+
+    return app
+
+
+app = setup_app()
 
