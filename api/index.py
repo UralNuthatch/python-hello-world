7bc388994dc75d2ac17ad303adb5736708dbc463
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-23 01:45:35 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,7 +14,7 @@ WEB_SERVER_HOST = "https://python-hello-world-uralnuthatchs-projects.vercel.app"
 # Порты сервера: 
 WEB_SERVER_PORT = 8350
 # Путь к маршруту вебхука, по которому Telegram будет отправлять запросы
-WEBHOOK_PATH = f''
+WEBHOOK_PATH = f'/bot/{getenv("BOT_TOKEN")}'
 # Базовый URL-адрес вебхука, который будет исп-ся для создания URL-адреса вебхука для Telegram
 BASE_WEBHOOK_URL = f"{WEB_SERVER_HOST}{WEBHOOK_PATH}"
 # На сервере только IPv6 (аналог ip4: 0.0.0.0).
@@ -60,10 +60,10 @@ async def send_echo(message: Message):
 @app.get("/")
 def setup():
     bot.set_webhook(url=BASE_WEBHOOK_URL, drop_pending_updates=True)
-    #requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text=Hello')
+    requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text=Hello')
     return "Webhook Updated"
 
-@app.post(BASE_WEBHOOK_URL)
+@app.post(WEBHOOK_PATH)
 async def bot_webhook(update: dict):
     res = await dp.feed_webhook_update(bot, update)
     requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text=POST')
