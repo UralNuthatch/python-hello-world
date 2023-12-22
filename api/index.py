9d6045b423996b817ca84d9574d7d1c0f5efbf4a
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-22 22:07:23 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,7 +16,7 @@ WEB_SERVER_HOST = "http://python-hello-world-uralnuthatchs-projects.vercel.app"
 # Порты сервера: 
 WEB_SERVER_PORT = 8350
 # Путь к маршруту вебхука, по которому Telegram будет отправлять запросы
-WEBHOOK_PATH = f'/bot/{getenv("BOT_TOKEN")}'
+WEBHOOK_PATH = f''
 # Базовый URL-адрес вебхука, который будет исп-ся для создания URL-адреса вебхука для Telegram
 BASE_WEBHOOK_URL = f"{WEB_SERVER_HOST}{WEBHOOK_PATH}"
 # На сервере только IPv6 (аналог ip4: 0.0.0.0).
@@ -61,7 +61,7 @@ async def send_echo(message: Message):
 
 @app.get("/")
 async def root():
-    #await bot.set_webhook(url=BASE_WEBHOOK_URL, drop_pending_updates=True)
+    await bot.set_webhook(url=BASE_WEBHOOK_URL, drop_pending_updates=True)
     return "Webhook Updated"
 
 @app.post(WEBHOOK_PATH)
