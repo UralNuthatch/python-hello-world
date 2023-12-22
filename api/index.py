bbb31629aa1e7fb25a2dbf6ded22cba8bd470295
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-23 00:05:43 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import logging
+import sys
 import requests
-import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
@@ -11,7 +11,7 @@ from fastapi import FastAPI
 
 # Настройки веб-сервера
 WEB_SERVER_HOST = "https://python-hello-world-uralnuthatchs-projects.vercel.app"
-# Порты сервера:
+# Порты сервера: 
 WEB_SERVER_PORT = 8350
 # Путь к маршруту вебхука, по которому Telegram будет отправлять запросы
 WEBHOOK_PATH = f''
@@ -29,48 +29,42 @@ logging.basicConfig(
             "%(funcName)s(): "
             "%(lineno)d:\t"
             "%(message)s",
-)
+)  
 logging.info("Application started")
 
+app = FastAPI()
 
-async def setup_app():
-    app = FastAPI()
+# Создаем объекты бота и диспетчера
+bot = Bot(token=getenv("BOT_TOKEN"), parse_mode='HTML')
+dp = Dispatcher()
 
-    # Создаем объекты бота и диспетчера
-    bot = Bot(token=getenv("BOT_TOKEN"), parse_mode='HTML')
-    dp = Dispatcher()
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-    # Этот хэндлер будет срабатывать на команду "/start"
-    @dp.message(Command(commands=["start"]))
-    async def process_start_command(message: Message):
-        await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ\nя пришлю тебе твое сообщение')
 
-    # Этот хэндлер будет срабатывать на команду "/help"
-    @dp.message(Command(commands=['help']))
-    async def process_help_command(message: Message):
-        await message.answer('Напиши мне что-нибудь и в ответ\nя пришлю тебе твое сообщение')
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text = 'Данный тип апдейтов не поддерживается методом send_copy')
 
-    # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения
-    # кроме команд "/start" и "/help"
-    @dp.message()
-    async def send_echo(message: Message):
-        try:
-            await message.send_copy(chat_id=message.chat.id)
-        except TypeError:
-            await message.reply(text = 'Данный тип апдейтов не поддерживается методом send_copy')
+@app.get("/")
+async def setup():
+    await bot.set_webhook(url=BASE_WEBHOOK_URL, drop_pending_updates=True)
+    requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text=Hello')
+    return "Webhook Updated"
 
-    @app.get("/")
-    async def setup():
-        await bot.set_webhook(url=BASE_WEBHOOK_URL, drop_pending_updates=True)
-        requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text=Hello')
-        return "Webhook Updated"
-
-    @app.post(WEBHOOK_PATH)
-    async def bot_webhook(update: dict):
-        res = await dp.feed_webhook_update(bot, update)
-        requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text=POST')
-        return res
-
-    return app
-
-app = asyncio.run(setup_app())
+@app.post(WEBHOOK_PATH)
+async def bot_webhook(update: dict):
+    res = await dp.feed_webhook_update(bot, update)
+    requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text=POST')
+    return res
