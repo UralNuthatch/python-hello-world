1e76ae024112c72c274a20b4a5f6b58822a26305
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-23 23:22:16 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,4 +7,27 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
+
+        # Настройки веб-сервера
+        WEB_SERVER_HOST = "https://python-hello-world-uralnuthatchs-projects.vercel.app"
+        # Порты сервера: 
+        WEB_SERVER_PORT = 8350
+        # Путь к маршруту вебхука, по которому Telegram будет отправлять запросы
+        WEBHOOK_PATH = f'/bot/{getenv("BOT_TOKEN")}'
+        # Базовый URL-адрес вебхука, который будет исп-ся для создания URL-адреса вебхука для Telegram
+        BASE_WEBHOOK_URL = f"{WEB_SERVER_HOST}{WEBHOOK_PATH}"
+        # На сервере только IPv6 (аналог ip4: 0.0.0.0).
+        WEBAPP_HOST = "0.0.0.0"
+
+        # Создаем объекты бота и диспетчера
+        bot = Bot(token=getenv("BOT_TOKEN"))
+        dp = Dispatcher()
+
+        async def setup():
+            await bot.set_webhook(url=BASE_WEBHOOK_URL, drop_pending_updates=True)
+
+        setup()
+
+        
+        
         return
