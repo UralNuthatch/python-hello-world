4434dde248c2e8c7ec987f671deeff0d31df01df
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-24 14:12:20 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -45,5 +45,5 @@ async def setup():
 @app.post(WEBHOOK_PATH)
 async def bot_webhook(update: dict):
     #res = await dp.feed_webhook_update(bot, update)
-    requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text={update["text"]}')
+    requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text={update["message"]["text"]}')
     #return res
