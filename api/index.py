d02f951daa2216126534f4f1e2df84300990eea7
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-25 21:47:10 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -44,12 +44,13 @@ async def send_echo(message: Message):
     except TypeError:
         await message.reply(text = 'Данный тип апдейтов не поддерживается методом send_copy')
 
-#@app.get("/")
-#async def setup():
+@app.get("/")
+async def setup():
 #    await bot.set_webhook(url=BASE_WEBHOOK_URL, drop_pending_updates=True)
 #    requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text=Hello')
-#    return "Webhook Updated"
+    return "Webhook Updated"
 
+@asynccontextmanager
 @app.post(WEBHOOK_PATH)
 async def bot_webhook(update: dict):
     telegram_update = types.Update(**update)
