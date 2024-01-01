ba6ea2e61c6375859565a445fb8c9def0344dfed
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2024-01-01 18:24:13 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,6 @@ from aiogram.filters import Command, CommandStart
 from os import getenv
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-import asyncio
 
 
 # Настройки веб-сервера
@@ -53,7 +52,5 @@ async def setup():
 
 @app.post(WEBHOOK_PATH)
 async def bot_webhook(update: dict):
-    telegram_update = Update(**update)
-    await dp.feed_webhook_update(bot, update=telegram_update)
-    #await asyncio.sleep(5)
+    #await dp.feed_webhook_update(bot, update)
     requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text={update}')
