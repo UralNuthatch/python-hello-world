3eff4b3a21bdbb37302f7e7165e4c498e1843f12
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2024-05-06 11:55:06 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -61,6 +61,6 @@ async def feed(update):
 async def bot_webhook(update: dict):
     await bot.send_message(chat_id=348123497, text="test")
     #await dp.feed_webhook_update(bot, update)
-    task = asyncio.get_event_loop().create_task(feed(update))
-    await task
+    #task = asyncio.get_event_loop().create_task(feed(update))
+    #await task
     requests.get(f'https://api.telegram.org/bot{getenv("BOT_TOKEN")}/sendMessage?chat_id=348123497&text={update}')
